b157deac987bbbf4e7cb27d6aa4a19f1d5e1e296
RaedGazi <64106244+RaedGazi@users.noreply.github.com>
2023-07-11 14:37:08 -0500

    Files added from the pharma_api_db folder from RG

---
+++ src/main/java/com/pharmatechsupply/pharma_api_db/PharmaApiDbApplication.java
@@ -0,0 +1,13 @@
+package com.pharmatechsupply.pharma_api_db;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PharmaApiDbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PharmaApiDbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pharmatechsupply/pharma_api_db/PharmaApiDbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pharmatechsupply.pharma_api_db;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PharmaApiDbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
